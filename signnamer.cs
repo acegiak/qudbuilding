46769bf4d74192b49721d91887cb0fbf2a23f838
Ashton McAllan <acegiak@gmail.com>
2019-08-19 21:17:34 +0930

    signs and torches

---
+++ signnamer.cs
@@ -0,0 +1,39 @@
+using System;
+using XRL.Core;
+using XRL.UI;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class acegiak_SignNamer : IPart
+	{
+
+		public override bool SameAs(IPart p)
+		{
+			return false;
+		}
+
+		public override bool AllowStaticRegistration()
+		{
+			return true;
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "EnterCell");
+			base.Register(Object);
+		}
+
+
+		public override bool FireEvent(Event E)
+		{
+			if (E.ID == "EnterCell")
+			{
+				if(ParentObject.GetPart<Chat>() != null){
+					ParentObject.GetPart<Chat>().Says = Popup.AskString("Write on sign...","",140);
+				}
+			}
+			return base.FireEvent(E);
+		}
+	}
+}
